46076f59bd33bb9169f160c64246486bcd619e89
Anton Nikolaev <migufa@mail.ru>
2022-12-27 14:18:02 +0500

    Итерация 3

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"github.com/labstack/echo/v4"
 	"io"
 	"math/rand"
 	"net/http"
+	"net/url"
 	"strconv"
 )
 
@@ -30,19 +32,21 @@ func main() {
 func createItem(c echo.Context) error {
 	defer c.Request().Body.Close()
 
-	//if c.Request().Body == nil {
-	//	return echo.NewHTTPError(http.StatusBadRequest, "body обязательно")
-	//}
-
 	body, err := io.ReadAll(c.Request().Body)
 	if err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, "Internal Server Error")
 	}
 
-	//_, err = url.ParseRequestURI(string(body))
-	//if err != nil {
-	//	echo.NewHTTPError(http.StatusBadRequest, "В body должен быть url")
-	//}
+	if len(body) == 0 {
+		return echo.NewHTTPError(http.StatusBadRequest, "Body обязательно")
+	}
+
+	_, err = url.ParseRequestURI(string(body))
+	if err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, "Невалидный url")
+
+	}
+	fmt.Printf("body: %s\n", string(body))
 
 	randomString := strconv.Itoa(rand.Int())
 	randomString = randomString[:6]
